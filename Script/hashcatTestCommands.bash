07b37e3ff957cb2a606dc6219d2f352af8c1a495
Tom Cinbis <tom.cinbis@unibas.ch>
2018-05-02 08:37:23 +0200

    Added script to test performance with different charsets

---
+++ Script/hashcatTestCommands.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# CPU tests
+./hashcat64.bin -a 3 -D 1 --force -m 2500 output.hccapx ?d?d?d?d?d?d?d?d # 8 digits only --> 6h 55min
+./hashcat64.bin -a 3 -D 1 --force -m 2500 output.hccapx ?l?l?l?l?l?l?l?l # 8 lower case letters only --> 1y 234d
+./hashcat64.bin -a 3 -D 1 --force -m 2500 output.hccapx ?u?u?u?u?u?u?u?u # 8 upper case letters only --> 1y 220d
+./hashcat64.bin -a 3 -D 1 --force -m 2500 output.hccapx ?a?a?a?a?a?a?a?a # 8 mixed characters --> NEXT BIG BAND (>10y)
+
+# GPU tests
+./hashcat64.bin -a 3 -D 2 -m 2500 output.hccapx ?d?d?d?d?d?d?d?d # 8 digits only
+./hashcat64.bin -a 3 -D 2 -m 2500 output.hccapx ?l?l?l?l?l?l?l?l # 8 lower case letters only
+./hashcat64.bin -a 3 -D 2 -m 2500 output.hccapx ?u?u?u?u?u?u?u?u # 8 upper case letters only
+./hashcat64.bin -a 3 -D 2 -m 2500 output.hccapx ?a?a?a?a?a?a?a?a # 8 mixed characters
