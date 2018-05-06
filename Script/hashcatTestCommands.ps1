e7e4a6f71cdc067a8320999be5b3f3a56d583df2
MaxBurgert <max.burgert@unibas.ch>
2018-05-06 15:13:33 +0200

    Typo fix

--- Script/hashcatTestCommands.ps1
+++ Script/hashcatTestCommands.ps1
@@ -2,7 +2,7 @@
 ./hashcat64.exe -a 3 -D 1 --force -m 2500 output.hccapx ?d?d?d?d?d?d?d?d # 8 digits only --> 6h 55min
 ./hashcat64.exe -a 3 -D 1 --force -m 2500 output.hccapx ?l?l?l?l?l?l?l?l # 8 lower case letters only --> 1y 234d
 ./hashcat64.exe -a 3 -D 1 --force -m 2500 output.hccapx ?u?u?u?u?u?u?u?u # 8 upper case letters only --> 1y 220d
-./hashcat64.exe -a 3 -D 1 --force -m 2500 output.hccapx ?a?a?a?a?a?a?a?a # 8 mixed characters --> NEXT BIG BAND (>10y)
+./hashcat64.exe -a 3 -D 1 --force -m 2500 output.hccapx ?a?a?a?a?a?a?a?a # 8 mixed characters --> NEXT BIG BANG (>10y)
 
 # GPU tests
 ./hashcat64.exe -a 3 -D 2 -m 2500 output.hccapx ?d?d?d?d?d?d?d?d # 8 digits only
